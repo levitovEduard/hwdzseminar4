9e7ee51a1d9cf9100fd1689b238230293c429b6b
Eduard <levitov.eduard@gmail.com>
2022-12-14 20:48:11 +0200

    zadacha 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+Console.WriteLine(" Inpur a number ");
+   
+   for (int i = 0; i < array.Length; i++)
+    
+     {
+       
+       int a = Convert.ToInt32(Console.ReadLine());
+       array[i] = a;
+     }
+   
+    for (int i = 0; i < array.Length; i++)
+
+       
+
+Console.Write( "{0} ", array[i]);
